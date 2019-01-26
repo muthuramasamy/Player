1fd9b89ecdbebc48f39ee8bf5c183250f1622c08
muthuramasamy <40240197+muthuramasamy@users.noreply.github.com>
2019-01-26 17:17:52 +0530

    Create k_shifting.py

---
+++ k_shifting.py
@@ -0,0 +1,17 @@
+n,k = map(int,input().split())
+l = list(map(int,input().split()))
+if(n<k):
+    for i in range(n):
+        if(i == n-1):
+            print(lis[i],end="")
+        else:
+            print(lis[i],end=" ")
+else:
+    a= n-k
+    for i in range(a,n):
+        print(l[i],end = " ")
+    for i in range(0,a):
+        if(i==a-1):
+            print(l[i],end="")
+        else:
+            print(l[i],end = " ")
