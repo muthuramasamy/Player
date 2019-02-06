d3ac02e711d921d99279ebc1b5604932c0e80bb0
muthuramasamy <40240197+muthuramasamy@users.noreply.github.com>
2019-02-06 20:03:16 +0530

    Update odd_factors.py

--- odd_factors.py
+++ odd_factors.py
@@ -1,15 +1,11 @@
-# your code goes 
 #guvi
 n=int(input())
-l2=[]
-for i in range(1,n+1):
+f=0
+for i in range(1,n+1,2):
 	if n%i==0:
-		l2.append(i)
-	else:
-		continue
-for i in range(len(l2)):
-	if i==len(l2)-1:
-		print(l2[i],end="")
-	else:
-		print(l2[i],end=" ")
+		if f==0:
+			print(i,end="")
+			f+=1
+		else:
+			print("",i,end="")
 	
